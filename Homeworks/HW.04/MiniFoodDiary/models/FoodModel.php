24f2c3a78578a6eeae153372b12469a060212e7d
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-03-23 17:29:54 +0300

    hw-04 done

--- Homeworks/HW.04/MiniFoodDiary/models/FoodModel.php
+++ Homeworks/HW.04/MiniFoodDiary/models/FoodModel.php
@@ -1,24 +1,15 @@
 <?php
-
 class FoodModel {
-    private $dataFile;
-
-    public function __construct($dataFile) {
-        $this->dataFile = $dataFile;
-        $this->initializeStorage();
-    }
-    
-    private function initializeStorage() {
-        if (!file_exists($this->dataFile)) {
-            file_put_contents($this->dataFile, json_encode([], JSON_PRETTY_PRINT));
+    public function __construct() {
+        if (!isset($_SESSION['foods'])) {
+            $_SESSION['foods'] = [];
         }
     }
-
+    
     public function getAll() {
-        $content = file_get_contents($this->dataFile);
-        return json_decode($content, true) ?: [];
+        return $_SESSION['foods'] ?? [];
     }
-
+    
     public function getById($id) {
         $foods = $this->getAll();
         foreach ($foods as $food) {
@@ -28,25 +19,26 @@ class FoodModel {
         }
         return null;
     }
-
+    
     public function create($data) {
         $foods = $this->getAll();
-
+        
         $newId = count($foods) > 0 ? max(array_column($foods, 'id')) + 1 : 1;
-
+        
         $foods[] = [
             'id' => $newId,
             'title' => $data['title'],
             'description' => $data['description'],
-            'fats' => (float) $data['fats'],
             'proteins' => (float) $data['proteins'],
+            'fats' => (float) $data['fats'],
             'carbohydrates' => (float) $data['carbohydrates'],
         ];
-
-        $this->save($foods);
+        
+        $_SESSION['foods'] = $foods;
+        
         return $newId;
-
     }
+    
     public function update($id, $data) {
         $foods = $this->getAll();
         
@@ -54,33 +46,14 @@ class FoodModel {
             if ($food['id'] == $id) {
                 $foods[$key]['title'] = $data['title'];
                 $foods[$key]['description'] = $data['description'];
-                $foods[$key]['fats'] = (int) $data['fats'];
-                $foods[$key]['proteins'] = (int) $data['proteins'];
-                $foods[$key]['carbohydrates'] = (int) $data['carbohydrates'];
+                $foods[$key]['proteins'] = (float) $data['proteins'];
+                $foods[$key]['fats'] = (float) $data['fats'];
+                $foods[$key]['carbohydrates'] = (float) $data['carbohydrates'];
                 break;
             }
         }
         
-        $this->save($foods);
+        $_SESSION['foods'] = $foods;
         return true;
     }
-    // public function delete($id) {
-    //     $foods = $this->getAll();
-        
-    //     foreach ($foods as $key => $food) {
-    //         if ($food['id'] == $id) {
-    //             unset($foods[$key]);
-    //             break;
-    //         }
-    //     }
-        
-    //     $this->save(array_values($foods));
-    //     return true;
-    // }
-
-    private function save($foods) {
-        file_put_contents($this->dataFile, json_encode($foods, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-    }
 }
-
-?>
